9d7323bcbbfcf32d71d57daa0c1ce864da037f6a
khalil-codes <khalilpati55@gmail.com>
2022-03-15 00:50:15 +0530

    Added an event emitter to join room using socket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import io from "socket.io-client";
 import "./App.css";
 
+const socket = io("http://localhost:5000");
+
 function App() {
-    return <div className="App">Hello World</div>;
+    const [name, setName] = useState<string>("");
+    const [roomID, setRoomID] = useState<string>("");
+    const [isFormFilled, setIsFormFilled] = useState(false);
+    useEffect(() => {
+        if (roomID && name) setIsFormFilled(true);
+    }, [name, roomID]);
+    const joinRoom = () => {
+        console.log(name, roomID);
+        socket.emit("join_room", roomID);
+    };
+    return (
+        <div className="App">
+            <div className="joinChatContainer">
+                <h3>Join A Chat</h3>
+                <input
+                    type="text"
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        setName(e.target.value)
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="1111111"
+                    value={roomID}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        setRoomID(e.target.value)
+                    }
+                />
+                <button onClick={joinRoom} disabled={!isFormFilled}>
+                    Join
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
